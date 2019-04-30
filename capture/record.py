6791f0b0833f4cf91ff2f847088e351cda7d4917
Alexey Ivchenko <me@swdevelop.net>
2019-04-30 18:14:22 -0400

    Add fake video stream mode; Debug real & fake mode

--- capture/record.py
+++ capture/record.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 import subprocess as sp
 from time import sleep
 
-
 LENGTH_SECONDS = 10
 
 USER = 'USER'
@@ -17,12 +16,14 @@ OUT_FILE = '_cam999.mov'
 # openRTSP -q -D 1 -B 10000000 -b 10000000 -Q -d 10 -t -u <S_LOGIN> 
 # <S_PASSWORD> rtsp://192.168.0.ABC:554/profileX/ > vid123.mov
 
+FAKE = True
+
 COMMAND_BASE = ('openRTSP '
                 '-q -D 1 -B 10000000 -b 10000000 -Q -d 10 -t'
                 ' -u %s %s %s' % (USER, PWD, RTSP_URL))
 
 # video data emulation, this outputs 1mb of rand data:
-COMMAND_DASE = 'head -c 1M < /dev/urandom'
+COMMAND_BASE_FAKE = 'head -c 1M'
 
 def date_to_str(date):
     return '_'.join(str(date).split(
@@ -34,15 +35,16 @@ def record():
     timestamp_end = timestamp_start + timedelta(seconds=LENGTH_SECONDS)
     end_str = date_to_str(timestamp_end)
 
-    filename = start_str + '___' + end_str
-    print(filename)
-    exec_cmd = COMMAND_BASE.split()[0]
-    exec_cmd_args = COMMAND_BASE.split()[1:]
-    exec_cmd_args[-1] += filename + OUT_FILE
+    filename = start_str + '___' + end_str + OUT_FILE
 
     # execution:
     with open(filename, 'wb') as out_fl:
-        sp.call([exec_cmd] + exec_cmd_args, stdout=out_fl)
+        if not FAKE:
+            sp.call(COMMAND_BASE.split(), stdout=out_fl)
+        else:
+            with open('/dev/urandom', 'rb') as rand_stream:
+                sp.call(COMMAND_BASE_FAKE.split(),
+                        stdin=rand_stream, stdout=out_fl)
 
     sleep(2)
 
